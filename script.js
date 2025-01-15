071e880a5aee923e1054a833247a0daec0436c81
AniketSharma-dev <aniketsharma.xo@gmail.com>
2025-01-15 21:17:29 +0530

    Theme Chnging

--- script.js
+++ script.js
@@ -1,51 +1,51 @@
 function Typingtext() {
-    const typingText = [
-        " Electric type Pokémon",
-        " Water type Pokémon",
-        " Fire type Pokémon",
-    ];
-    const typingSpeed = 100;
-    const eraseSpeed = 50;
-    const newTextDelay = 2000;
-    const textContainer = document.getElementById("typing-text");
+  const typingText = [
+    " Electric type Pokémon",
+    " Cute Pokémon",
+    " Yellow and orange color Pokémon",
+  ];
+  const typingSpeed = 100;
+  const eraseSpeed = 50;
+  const newTextDelay = 2000;
+  const textContainer = document.getElementById("typing-text");
 
-    if (!textContainer) {
-        console.error("Element with id 'typing-text' not found.");
-        return;
+  if (!textContainer) {
+    console.error("Element with id 'typing-text' not found.");
+    return;
+  }
+
+  let textArrayIndex = 0;
+  let charIndex = 0;
+  let isDeleting = false;
+
+  function type() {
+    const currentText = typingText[textArrayIndex];
+    // typing
+    if (!isDeleting && charIndex < currentText.length) {
+      textContainer.textContent += currentText.charAt(charIndex);
+      charIndex++;
+      setTimeout(type, typingSpeed);
     }
-
-    let textArrayIndex = 0;
-    let charIndex = 0;
-    let isDeleting = false;
-
-    function type() {
-        const currentText = typingText[textArrayIndex];
-        // typing
-        if (!isDeleting && charIndex < currentText.length) {
-            textContainer.textContent += currentText.charAt(charIndex);
-            charIndex++;
-            setTimeout(type, typingSpeed);
-        }
-        // deleting
-        else if (isDeleting && charIndex > 0) {
-            textContainer.textContent = currentText.substring(0, charIndex - 1);
-            charIndex--;
-            setTimeout(type, eraseSpeed);
-        }
-        // switch to next text
-        else {
-            if (!isDeleting) {
-                isDeleting = true;
-                setTimeout(type, newTextDelay);
-            } else {
-                isDeleting = false;
-                textArrayIndex = (textArrayIndex + 1) % typingText.length;
-                charIndex = 0;
-                setTimeout(type, typingSpeed);
-            }
-        }
+    // deleting
+    else if (isDeleting && charIndex > 0) {
+      textContainer.textContent = currentText.substring(0, charIndex - 1);
+      charIndex--;
+      setTimeout(type, eraseSpeed);
     }
-    type();
+    // switch to next text
+    else {
+      if (!isDeleting) {
+        isDeleting = true;
+        setTimeout(type, newTextDelay);
+      } else {
+        isDeleting = false;
+        textArrayIndex = (textArrayIndex + 1) % typingText.length;
+        charIndex = 0;
+        setTimeout(type, typingSpeed);
+      }
+    }
+  }
+  type();
 }
 
 function sConsole(e) {
